8fb93a3937abf55af37752c88dbe5101d8007667
315-07 <pasha08.games@gmail.com>
2023-09-16 10:18:11 +0300

    Update

--- less11.py
+++ less11.py
@@ -1,6 +1,24 @@
 import cv2
+from PIL import Image
 
 image_cat = "cat.jpeg"
 image = cv2.imread(image_cat)
-cv2.imshow("Cat", image)
+
+cat_cascade = cv2.CascadeClassifier("haarcascade_frontalcatface_extended.xml")
+
+cat_face = cat_cascade.detectMultiScale(image)
+print(cat_face)
+
+cat = Image.open(image_cat)
+glasses = Image.open('glasses.png')
+cat = cat.convert("RGBA")
+glasses = glasses.convert("RGBA")
+
+for (x, y, w, h) in cat_face:
+    glasses = glasses.resize((w, int(h / 3)))
+    cat.paste(glasses, (x, int(y + h / 4)), glasses)
+
+cat.save("kit911.png")
+kit911 = cv2.imread("kit911.png")
+cv2.imshow("Cat", kit911)
 cv2.waitKey()
